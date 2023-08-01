04e98a11af6da2576bea92bd9b3649f6080ac225
Lalitpatida <lalitpatidar244@gmail.com>
2023-08-02 00:41:37 +0530

    added timestamp

---
+++ routes/hotel.router.js
@@ -0,0 +1,12 @@
+const express =require('express');
+const router=express.Router();
+
+const getAllHotelHandler = require('../controllers/hotelController');
+
+
+
+router.route("/") //localhost:3500/api/hotels/hotelCategory
+.get(getAllHotelHandler)
+
+
+module.exports=router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const mongoose=require('mongoose');
+
+const hotelDataAddedToDBRouter=require("./routes/dataimport.router");
+const categoryDataAddedToDBRouter=require("./routes/categoryimport.router");
+
+const hotelRouter=require("./routes/hotel.router");
+const categoryRouter=require("./routes/category.router");
+const singleHotelRouter=require("./routes/singlehotel.router");
+const authRouter=require("./routes/auth.router");
+const wishlistRouter=require("./routes/wishlist.router");
+
+
+const connectDB=require("./config/dbconfig");
+
+const app=express();
+
+app.use(express.json());
+connectDB();
+
+const PORT=3500;
+
+app.get("/",(req,res)=>{
+    res.send("Hello lala")
+})
+
+
+app.use("/api/hoteldata",hotelDataAddedToDBRouter);
+app.use("/api/categorydata",categoryDataAddedToDBRouter);
+app.use("/api/category",categoryRouter);
+app.use("/api/hotels",hotelRouter);
+app.use("/api/hotels",singleHotelRouter);
+app.use("/api/auth",authRouter);
+app.use("/api/wishlist",wishlistRouter);
+
+mongoose.connection.once("open",()=>{
+    console.log("connected to DB");
+    app.listen(process.env.PORT || PORT,()=>{
+        console.log("server is Up and running")
+    })
+})
+
